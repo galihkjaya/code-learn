03c2e250088808a93bf872fd78f2136a865d7966
galihkjaya <galihkjaya15@gmail.com>
2026-06-21 17:33:05 +0700

    feat: init vite react ts project

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <main>
+      <h1>CodeLearn</h1>
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
